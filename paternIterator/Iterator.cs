ec6c39d665a4696d74faf297f94afdfd86978e8b
PatsoraFI-13 <patsorapolina@icloud.com>
2023-05-24 14:31:03 +0300

    1

---
+++ paternIterator/Iterator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+
+public interface IIterator
+{
+    bool hasMore();
+    object getNext();
+}
+
+public class ConcreteIterator : IIterator
+{
+    private readonly ConcreteCollection collection;
+    private int position = 0;
+    public ConcreteIterator(ConcreteCollection collection)
+    {
+        this.collection = collection;
+    }
+    public bool hasMore()
+    {
+        return position < collection.Count;
+    }
+    public object getNext()
+    {
+        if (hasMore())
+        {
+            object item = collection[position];
+            position++;
+            return item;
+        }
+        throw new IndexOutOfRangeException("No more elements");
+    }
+}
+
+public interface ICollection
+{
+    IIterator CreateIterator();
+}
+
+public class ConcreteCollection : ICollection
+{
+    private readonly ArrayList items = new ArrayList();
+    public object this[int index]
+    {
+        get { return items[index]; }
+        set { items.Insert(index, value); }
+    }
+    public int Count
+    {
+        get { return items.Count; }
+    }
+    public IIterator CreateIterator()
+    {
+        return new ConcreteIterator(this);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConcreteCollection collection = new ConcreteCollection();
+        collection[0] = "Item 1";
+        collection[1] = "Item 2";
+        collection[2] = "Item 3";
+        collection[3] = "Item 4";
+        collection[4] = "Item 5";
+
+        IIterator iterator = collection.CreateIterator();
+        while (iterator.hasMore())
+        {
+            object item = iterator.getNext();
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ paternIterator/IteratorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class IteratorTests
+{
+    [Test]
+    public void ConcreteIterator_HasNext_ReturnsTrueWhenItemsAvailable()
+    {
+        ConcreteCollection collection = new ConcreteCollection();
+        collection[0] = "Item 1";
+
+        ConcreteIterator iterator = new ConcreteIterator(collection);
+        bool hasNext = iterator.hasMore();
+        Assert.IsTrue(hasNext);
+    }
+
+    [Test]
+    public void ConcreteIterator_HasNext_ReturnsFalseWhenNoItemsAvailable()
+    {
+        ConcreteCollection collection = new ConcreteCollection();
+        ConcreteIterator iterator = new ConcreteIterator(collection);
+        bool hasNext = iterator.hasMore();
+        Assert.IsFalse(hasNext);
+    }
+
+    [Test]
+    public void ConcreteIterator_Next_ReturnsNextItem()
+    {
+        ConcreteCollection collection = new ConcreteCollection();
+        collection[0] = "Item 1";
+        collection[1] = "Item 2";
+
+        ConcreteIterator iterator = new ConcreteIterator(collection);
+        object nextItem = iterator.getNext();
+        Assert.AreEqual("Item 1", nextItem);
+    }
+
+    [Test]
+    public void ConcreteIterator_Next_ThrowsExceptionWhenNoMoreElements()
+    {
+        ConcreteCollection collection = new ConcreteCollection();
+        ConcreteIterator iterator = new ConcreteIterator(collection);
+        Assert.Throws<IndexOutOfRangeException>(() => iterator.getNext());
+    }
+}
